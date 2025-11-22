8f283f5beab634c7c1cc1bc63db0776b87d315b3
Franz <franzquarshie@gmail.com>
2025-11-22 08:42:55 -0300

    add CORS middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 
 // Health check endpoint
